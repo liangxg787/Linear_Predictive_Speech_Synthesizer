ba9d83c939a7f7aa1f9fc0d372d3191c595d8e5e
Ngene <hplxg@hotmail.com>
2024-10-29 21:26:17 +0000

    create repository

---
+++ main.m
@@ -0,0 +1,8 @@
+%% Author: Xiaoguang Liang (PG/T - Comp Sci & Elec Eng)
+%% University of Surrey, United Kingdom
+%% Email address: xl01339@surrey.ac.uk
+%% Time: 29/10/2024 21:16
+
+clc;
+close all;
+clear;
